4c413a661d413da35e7b6304c25fc7362a890781
vladislemon <vladislemon@gmail.com>
2025-05-26 09:57:17 +0600

    Fix checkstyle problems

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/mapwriter/IEMineralsMwDataProvider.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/mapwriter/IEMineralsMwDataProvider.java
@@ -1,6 +1,6 @@
 package blusunrize.immersiveengineering.common.util.compat.mapwriter;
 
-import java.awt.*;
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
