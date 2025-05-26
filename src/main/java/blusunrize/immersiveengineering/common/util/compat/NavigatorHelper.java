ad9d55d45255a968dccec0b63fe785be237939bf
vladislemon <vladislemon@gmail.com>
2025-05-26 10:13:48 +0600

    Disable Navigator integration when on server

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/NavigatorHelper.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/NavigatorHelper.java
@@ -1,20 +1,24 @@
 package blusunrize.immersiveengineering.common.util.compat;
 
-import com.gtnewhorizons.navigator.api.NavigatorApi;
-
 import blusunrize.immersiveengineering.common.util.compat.navigator.MineralsButtonManager;
 import blusunrize.immersiveengineering.common.util.compat.navigator.MineralsLayerManager;
+import com.gtnewhorizons.navigator.api.NavigatorApi;
+import cpw.mods.fml.common.FMLCommonHandler;
 
 public class NavigatorHelper extends IECompatModule {
 
     @Override
-    public void preInit() {}
-
-    @Override
-    public void init() {
-        NavigatorApi.registerLayerManager(new MineralsLayerManager(new MineralsButtonManager()));
+    public void preInit() {
     }
 
     @Override
-    public void postInit() {}
+    public void init() {
+        if (FMLCommonHandler.instance().getSide().isClient()) {
+            NavigatorApi.registerLayerManager(new MineralsLayerManager(new MineralsButtonManager()));
+        }
+    }
+
+    @Override
+    public void postInit() {
+    }
 }
